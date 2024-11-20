f04be67e557a9b5fe9e1d6c2a0a0ffd526ac7356
KORAHA <alsgh6562@gmail.com>
2024-11-20 12:11:07 +0900

    SBP02_WebStatic 정적리소스 사용하기 실습(HTML, CSS, JS, Image)

---
+++ SBP02_WebStatic/src/main/resources/static/script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#goSubMain").click(function() {
+        location.href = "/sub/subMain.html";
+    });
+
+    $("#goMain").click(function() {
+        location.href = "/index.html";
+    });
+});
